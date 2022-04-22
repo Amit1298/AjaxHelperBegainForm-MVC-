d1d879a7f3e66f1c57f07e316207ccea2dea6b53
Amit <amitkr24031995@gmail.com>
2022-04-22 16:47:25 +0530

    Adding AjaxHelperBegainForm

---
+++ AjaxHelperBegainForm/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using AjaxHelperBegainForm.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AjaxHelperBegainForm.Controllers
+{
+    public class HomeController : Controller
+    {
+        EmployeeDBEntities db = new EmployeeDBEntities();
+        // GET: Home
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(employee e)
+        {
+            if(ModelState.IsValid == true)
+            {
+                db.employees.Add(e);
+                int a = db.SaveChanges();
+                if (a > 0)
+                {
+                    return Json("Data Inserted");   //JavaScript Object Notation
+                }
+                else
+                {
+                    return Json("Data not Inserted");
+                }
+            }
+            return View();
+        }
+    }
+}
